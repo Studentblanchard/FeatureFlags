e3bb29ae4df7efd482939376ad45f2874da51d7a
Trevor Blanchard <trevor@IBEX-Pursuit.Intra>
2018-05-06 14:34:43 -0500

    Initial Commit

---
+++ example.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/patrickmn/go-cache"
+	"time"
+)
+const (
+	Enabled 	= "enabled"
+	Disabled 	= "disabled"
+)
+
+type FeatureFlag struct {
+	Name	string
+	State string
+}
+
+func GetFlags(ch *cache.Cache) []*FeatureFlag {
+	m := make([]*FeatureFlag, ch.ItemCount())
+	i := 0
+	for _, v := range ch.Items() {
+		ff, ok := v.Object.(*FeatureFlag)
+		if ok {
+			m[i] = ff
+			i = i + 1
+		}
+	}
+	return m
+}
+
+func main() {
+	r := gin.Default()
+	ch := cache.New(5*time.Minute, 10*time.Minute)
+
+	r.POST("/featureflag/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		state := c.DefaultPostForm("state", "disabled")
+
+		if state != Enabled && state != Disabled {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "unknown state: `" + state + "`. Valid states are `" + Enabled + "` and `" + Disabled + "`"})
+		} else {
+			ff := FeatureFlag{Name: name, State: state}
+			ch.Set("ff-" + name, &ff, cache.NoExpiration)
+			c.JSON(http.StatusOK, gin.H{"name": ff.Name, "state": ff.State})
+		}
+	})
+
+	r.DELETE("/featureflag/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		ch.Delete("ff-" + name)
+		c.JSON(http.StatusOK, gin.H{"name": name})
+	})
+
+	r.GET("/featureflag/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		ff, exist := ch.Get("ff-" + name)
+		if exist {
+			c.JSON(http.StatusOK, ff)
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{"error": name + " does not exist"})
+		}
+	})
+
+	r.GET("/featureflag", func(c *gin.Context) {
+		c.JSON(http.StatusOK, GetFlags(ch))
+	})
+
+	r.Run(":8080")
+}
